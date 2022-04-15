a0a833d49902613d2fc238dd7584f8217f39b965
Yash <yjain8958372013@gmail.com>
2022-04-15 21:54:05 +0530

    Added homepage animation

--- Portfolio-Using-React/src/components/Home/index.js
+++ Portfolio-Using-React/src/components/Home/index.js
@@ -3,9 +3,22 @@ import LogoTitle from "../../assets/images/home-image-4.png";
 import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
 import animationData from "../../lottie/lottie-2.json";
+import { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 
 
 const Home = () => {
+
+    const [letterClass,setLetterClass] = useState('text-animate');
+    const nameArray = ['a','s','h'];
+    const jobArray = ['w','e','b', ' ','d','e','v','e','l','o','p','e','r','.'];
+
+    useEffect(()=>{
+        return setTimeout(()=>{
+            setLetterClass('text-animate');
+        },4000);
+    })
+    
     const defaultOptions = {
         loop: true,
         autoplay: true,
@@ -14,23 +27,40 @@ const Home = () => {
           preserveAspectRatio: "xMidYMid slice"
         }
       };
+
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm 
-                <img src={LogoTitle} alt="My name is yash"/>
-                ash
+                <h1>
+                <span className={`${letterClass}`}>H</span>
+                <span className={`${letterClass} _10`}>i,</span>
+
                 <br/>
-                web developer
+                <span className={`${letterClass} _11`}>I</span>
+                <span className={`${letterClass} _12`}>'m</span>
+    
+                <img src={LogoTitle} alt="My name is yash"/>
+
+                <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={15}
+                />
+
+                <br/>
+                <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={22}
+                />
+
                 </h1>
+
                 <h2>
                     MERN Developer | UI-UX Designer | Compettive Programmer
                 </h2>
+
                 <Link to="/contact" className="contact-button">Contact Me</Link>
                 <div className="lottie">
-                    <Lottie
-                        options={defaultOptions}
-                    />
+                    <Lottie options={defaultOptions}/>  
                 </div>
                     
             </div>
