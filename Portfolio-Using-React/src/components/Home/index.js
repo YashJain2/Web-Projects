0b57d96cc0ad1710e0c1eaa449ff38a3d3767770
Yash <yjain8958372013@gmail.com>
2022-04-15 17:24:36 +0530

    Added homepage animation

--- Portfolio-Using-React/src/components/Home/index.js
+++ Portfolio-Using-React/src/components/Home/index.js
@@ -1,8 +1,19 @@
 import "./index.scss";
 import LogoTitle from "../../assets/images/home-image-4.png";
 import { Link } from "react-router-dom";
+import Lottie from "react-lottie";
+import animationData from "../../lottie/lottie-2.json";
+
 
 const Home = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
     return(
         <div className="container home-page">
             <div className="text-zone">
@@ -16,6 +27,12 @@ const Home = () => {
                     MERN Developer | UI-UX Designer | Compettive Programmer
                 </h2>
                 <Link to="/contact" className="contact-button">Contact Me</Link>
+                <div className="lottie">
+                    <Lottie
+                        options={defaultOptions}
+                    />
+                </div>
+                    
             </div>
         </div>
     );
